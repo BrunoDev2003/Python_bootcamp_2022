11b04c0008ecb6d8c2017d57a2e09339613c2ade
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-09-23 17:36:09 +0000

    functions with inputs.

---
+++ Functions_with_inputs.py
@@ -0,0 +1,18 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("hello")
+  print("i'm bruno")
+  print("how are you?")
+
+greet()
+
+
+def greet_with_name(name):
+  print("hello")
+  print(f"i'm {name}")
+
+greet_with_name("Bruno")
