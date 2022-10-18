2fb8e527cfee4af36b4c21fef7d085c624325b16
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-18 15:56:51 -0300

    fazendo a logica do deposito

--- app.py
+++ app.py
@@ -8,3 +8,29 @@ menu = """
 
 
 saldo = 0
+limite = 200
+extrato = ""
+numero_saques = 0
+limite_saques = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+
+        input("qual valor que deseja depositar?")
+        if saldo < 0:
+            print("Saldo insuficiente")
+            break
+        else:
+            saldo += extrato 
+
+    elif opcao == "s":
+
+        print("Sacar")
+    elif opcao == "e":
+        print("Extrato")
+    elif opcao == "q":
+        print("Operação inválida, por favor selecione novamente")
+        break
