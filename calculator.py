337d32647db4a89fbc9de60ae8caa57ba9820268
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-11-03 18:44:21 +0000

    using a while loop to check if the user wants to continue or not

--- calculator.py
+++ calculator.py
@@ -28,20 +28,27 @@ operations = {
 num1 = int(input("Whats the first number?: "))
 num2 = int(input("Whats the second number?: "))
 
+should_continue = True
 
-for symbols in operations:
-  print(symbols)
+while should_continue:
+  
+  for symbols in operations:
+    print(symbols)
+  
+  operations_symbols = input("Pick an operation from the line above: ")
+  
+  calculation_function = operations[operations_symbols]
+  answer = calculation_function(num1,num2)
+  
+  print(f"{num1} {operations_symbols} {num2} = {answer}")
+  
+  operation_symbol = input("Pick another operation: ")
+  num3 = int(input("what's the next number?: "))
+  calculation_function = operations[operation_symbol] 
+  second_answer = calculation_function(answer, num3)
+  
+  print(f"{answer} {operation_symbol} {num3} = {second_answer}")
 
-operations_symbols = input("Pick an operation from the line above: ")
-
-calculation_function = operations[operations_symbols]
-answer = calculation_function(num1,num2)
-
-print(f"{num1} {operations_symbols} {num2} = {answer}")
-
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("what's the next number?: "))
-calculation_function = operations[operation_symbol] 
-second_answer = calculation_function(answer, num3)
-
-print(f"{answer} {operation_symbol} {num3} = {second_answer}")
+  should_continue = input("type 'y' to continue calculating with 8, or type 'n' to exit.: ")
+  if should_continue != 'y':
+    should_continue = False
