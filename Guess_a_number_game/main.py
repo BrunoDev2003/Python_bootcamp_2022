a955a7f1b195d6874055e814a8c5bedb0edc1789
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-11-24 19:20:42 +0000

    Validating the user's choice of a number.

--- Guess_a_number_game/main.py
+++ Guess_a_number_game/main.py
@@ -1,12 +1,22 @@
 from art import logo
+import random
+#from replit import clear
 
 list = [1,2,3,4,5,6,7,8,9,10]
+number = random.choice(list)
+
 
 def start_game():
   print(logo)
-  print('Welcome to the Number Guessing Game!')
-  print('I1m thinking of a number between 1 and 10')
-  dificulty = input(print("choose dificulty"))
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 10")
+  dificulty = input(print("choose dificulty. Type 'easy' or 'hard': "))
   if dificulty == 'hard':
     print("You have 5 attempts remaining to guess a number.")
-    while number in list
+    guess = input(print("Make a guess: "))
+    if guess > number:
+      print("Too high \n Guess again.")
+    if guess < number:
+      print("Too low \n Guess again.")
+      
+      
