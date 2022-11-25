e2d60c39896b17c9db7916ee1bb38240583e0585
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-11-25 20:42:06 +0000

    implementing a logic control over the user's guess attempts.

--- Guess_a_number_game/main.py
+++ Guess_a_number_game/main.py
@@ -5,6 +5,8 @@ import random
 list = [1,2,3,4,5,6,7,8,9,10]
 number = random.choice(list)
 
+attempts_hard = 5
+attempts_easy = 10
 
 def start_game():
   print(logo)
@@ -12,11 +14,16 @@ def start_game():
   print("I'm thinking of a number between 1 and 10")
   dificulty = input(print("choose dificulty. Type 'easy' or 'hard': "))
   if dificulty == 'hard':
-    print("You have 5 attempts remaining to guess a number.")
-    guess = input(print("Make a guess: "))
-    if guess > number:
-      print("Too high \n Guess again.")
-    if guess < number:
-      print("Too low \n Guess again.")
-      
+    while attempts_hard < 5:   
+      print(f"You have {attempts_hard} attempts remaining to guess a number.")
+      guess = input(print("Make a guess: "))
+      if guess > number:
+        attempts_hard + 1
+        print("Too high \n Guess again.")
+      if guess < number:
+        attempts_hard + 1
+        print("Too low \n Guess again.")
+
+      elif guess == number:
+        print("You got it!")
       
