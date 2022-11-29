ca3fb56aa2a89b0bd6d5e6de98e5197af83e1bec
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-11-29 19:02:39 +0000

    implementing the easy dificulty.

--- Guess_a_number_game/main.py
+++ Guess_a_number_game/main.py
@@ -7,23 +7,38 @@ number = random.choice(list)
 
 attempts_hard = 5
 attempts_easy = 10
+attempts = 0
 
-def start_game():
+
+def start_game(list):
   print(logo)
-  print("Welcome to the Number Guessing Game!")
-  print("I'm thinking of a number between 1 and 10")
+  print("Welcome to the Number Guessing Game!\n")
+  print("I'm thinking of a number between 1 and 10\n")
   dificulty = input(print("choose dificulty. Type 'easy' or 'hard': "))
   if dificulty == 'hard':
-    while attempts_hard < 5:   
+    while attempts < 5:   
       print(f"You have {attempts_hard} attempts remaining to guess a number.")
       guess = input(print("Make a guess: "))
       if guess > number:
-        attempts_hard + 1
+        attempts + 1
         print("Too high \n Guess again.")
       if guess < number:
-        attempts_hard + 1
+        attempts + 1
         print("Too low \n Guess again.")
 
       elif guess == number:
         print("You got it!")
-      
+
+        
+  elif dificulty == 'easy':
+    while attempts < 10:   
+      print(f"You have {attempts_easy} attempts remaining to guess a number.")
+      guess = input(print("Make a guess: "))
+      if guess >= number:
+        attempts + 1
+        print("Too high \n Guess again.")
+      if guess <= number:
+        attempts + 1
+        print("Too low \n Guess again.")
+
+start_game(list)
