032819bf5df70086ee4c872f72ef3077681148e7
Bruno Gusmão <brunogusmao43@gmail.com>
2023-01-10 16:23:30 -0300

    implementing the resources accountability

--- main.py
+++ main.py
@@ -33,14 +33,40 @@ resources = {
 # TODO: 1.Print report of coffe machine recoursces
 
 
-
-def coffee_choice():
-    user_choice = input("What would you like? (espresso/latte/capuccino): " + user_choice)
+user_choice = input("What would you like? (espresso/latte/capuccino): " + user_choice)
+coins_inserted = 0
+    
     
 def report():
     print(MENU.espresso)
     print(MENU.latte)
     print(MENU.capuccino)
+
+def sufficient_resources():
+    if user_choice == "espresso":
+        if "water" <= 0:
+            print("Sorry there is not enough water.")
+        elif "coffe" <= 0:
+            print("Sorry there is not enough coffee.")
+    if user_choice == "latte":
+        if "water" <= 0:
+            print("Sorry there is not enough water.")
+        elif "milk" <= 0:
+            print("Sorry there is not enough milk.")
+        elif "coffe" <= 0:
+            print("Sorry there is not enough coffee.")
+    if user_choice == "capuccino":
+        if "water" <= 0:
+            print("Sorry there is not enough water.")
+        elif "milk" <= 0:
+            print("Sorry there is not enough milk.")
+        elif "coffe" <= 0:
+            print("Sorry there is not enough coffee.")
+
+
+def process_coins():
+
+#TODO: IMPLEMENTAR A FUNÇÃO PROCESS_COINS, E O CHECK TRANSACTION,E POR FIM O MAKE_COFFE
 
 
 def coffee_machine(argument):
