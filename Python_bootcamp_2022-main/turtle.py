8c345865e6f7fd298042192d8d1aa85817c291fd
Bruno Gusmão <brunogusmao43@gmail.com>
2023-04-19 15:50:32 -0300

    making a square with python turtle

--- Python_bootcamp_2022-main/turtle.py
+++
@@ -1,5 +0,0 @@
-from turtle import Turtle, Screen
-
-bruno_turtle = Turtle()
-bruno_turtle.shape()
-bruno_turtle.color("purple2")
---
+++ Python_bootcamp_2022-main/turtle_test.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+
+bruno_turtle = Turtle()
+
+
+for _ in range(4):
+    bruno_turtle.forward(100)
+    bruno_turtle.left(90)
+input("Press any key to exit ...")
