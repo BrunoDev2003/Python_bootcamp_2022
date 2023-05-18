201bd819a5d83da7bdc91c6e2107ef05e451d23b
Bruno Gusmão <brunogusmao43@gmail.com>
2023-05-18 17:02:06 -0300

    making the Etch a Sketch APP

--- Python_bootcamp_2022-main/python_event_listeners.py
+++ Python_bootcamp_2022-main/python_event_listeners.py
@@ -7,7 +7,23 @@ screen = Screen()
 def move_forward():
     bruno.forward(10)
 
+def move_backwards():
+    bruno.backward(10)
+def turn_right():
+    bruno.right(10)
+
+def turn_left():
+    bruno.left(10)
+
+def clear():
+    bruno.clear()
+    bruno.home()
+    bruno.clear()
 
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(move_forward, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
