2c654b11c1968d8bacc2710951e91768ff9f78c3
Bruno Gusmão <brunogusmao43@gmail.com>
2023-05-23 16:35:46 -0300

    finshing the race project

--- Python_bootcamp_2022-main/turtle_race.py
+++ Python_bootcamp_2022-main/turtle_race.py
@@ -5,12 +5,12 @@ import random
 is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
-
+all_turtles = []
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 
-for turtle in range(0, 6):
+for turtle in range(0, 6): 
     bruno = Turtle(shape="turtle")
     bruno.color(colors[turtle])
     bruno.penup()
@@ -20,8 +20,16 @@ if user_bet:
     is_race_on: True
 
 while is_race_on:
-    rand_distance = random.randint(0, 10)
-    turtle.foward(rand_distance)
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner of the race!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the loser of the race!")
+        rand_distance = random.randint(0, 10)
+        turtle.foward(rand_distance)
 
 
 
