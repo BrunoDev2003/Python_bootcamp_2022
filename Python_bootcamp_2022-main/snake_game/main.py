074e38433d707a495182c12b5ee92f567ac916df
Bruno Gusmão <brunogusmao43@gmail.com>
2023-05-25 17:14:49 -0300

    creating the snake body

---
+++ Python_bootcamp_2022-main/snake_game/main.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+
+screen.setup(width=800, height=800)
+screen.bgcolor("green")
+screen.title("SNAAAAAKEEEEE")
+
+t1 = Turtle()
+t2 = Turtle()
+t3 = Turtle()
+
+t1.color("white")
+t2.color("white")
+t3.color("white")
+
+t1.goto(-20, 0)
+t2.goto(-40,0)
+t3.goto(-60,0)
+
+t1.pensize(20)
+t2.pensize(20)
+t3.pensize(20)
+
+
+
+screen.exitonclick()
