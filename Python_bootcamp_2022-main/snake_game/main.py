ed716b768c0f06ffb7d63c701c8c5b3f1ce090d7
Bruno Gusmão <brunogusmao43@gmail.com>
2023-06-05 17:45:24 -0300

    finishing the previous work

--- Python_bootcamp_2022-main/snake_game/main.py
+++ Python_bootcamp_2022-main/snake_game/main.py
@@ -27,10 +27,12 @@ while game_on:
         screen.update()
         time.sleep(1)
 
-        for seg_num in range(start = len(segments) - 1, stop = 3, step = -1):
+        for seg_num in range(len(segments) - 1,0,-1):
             new_x = segments[seg_num - 1].xcor()
             new_y = segments[seg_num - 1].ycor()
-            segments[seg_num].goto(position)
+            segments[seg_num].goto(new_x, new_y)
+        segments[0].forward(20)
+        segments[0].left(90)
 
 
 
