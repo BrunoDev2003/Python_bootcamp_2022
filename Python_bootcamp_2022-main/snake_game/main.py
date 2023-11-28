52a7996e0a773bead3f12c9a4b7717a6a4cb1a82
Bruno Gusmão <brunogusmao43@gmail.com>
2023-11-28 17:04:25 -0300

    how to control the snake with a keypress

--- Python_bootcamp_2022-main/snake_game/main.py
+++ Python_bootcamp_2022-main/snake_game/main.py
@@ -4,12 +4,18 @@ import time
 
 screen = Screen()
 
+snake = Snake()
+
 screen.setup(width=800, height=800)
 screen.bgcolor("green")
 screen.title("SNAAAAAKEEEEE")
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
-snake = Snake()
 
 game_is_on = True
 while game_is_on:
